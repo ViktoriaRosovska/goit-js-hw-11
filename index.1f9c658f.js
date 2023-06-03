c9f9d95de34f2f114511aa8add87013ddab9365a
ViktoriaRosovska <121065008+ViktoriaRosovska@users.noreply.github.com>
2023-06-03 06:49:15 +0000

    Deploying to gh-pages from @ ViktoriaRosovska/goit-js-hw-11@19fc5186f5bdb61903c69af2409d70ff92af3db7 🚀

--- index.1f9c658f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,n;(e="dog",n=new URLSearchParams({key:"4959719-2280f709437ef1ad4f05c2184",q:e,image_type:"photo",orientation:"horizontal",safesearch:!0,page:1,per_page:40}),fetch("".concat("https://pixabay.com/api/","?").concat(n)).then((function(e){return e.json()}))).then((function(e){return console.log(e)}))}();
-//# sourceMappingURL=index.1f9c658f.js.map
