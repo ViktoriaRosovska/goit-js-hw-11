19fc5186f5bdb61903c69af2409d70ff92af3db7
Viktoria Rosovska <torimeister@gmail.com>
2023-06-03 09:48:44 +0300

    add gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
-import './js/api-service';
+import { fetchImages } from './js/api-service';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let page = 1;
+let searchTerm = '';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const link = document.querySelector('.image-link');
+
+
+form.addEventListener('submit', onSearchFormSubmit);
+function onSearchFormSubmit(evt) {
+    evt.preventDefault();
+    // console.log(evt.target.elements.searchQuery.value);
+    evt.target.elements.searchQuery.value = evt.target.elements.searchQuery.value.trim();
+    const inputSearch = evt.target.elements.searchQuery.value;
+    
+    if (!inputSearch || inputSearch === searchTerm) {
+        return;
+    }
+    searchTerm = inputSearch;
+    page = 1;
+    gallery.innerHTML = "";
+    getImages(searchTerm, page);
+    loadMoreBtn.classList.remove('js-invisible');
+}     
+        
+   
+
+async function getImages(searchTerm, page) {
+     const data = await fetchImages(searchTerm, page);
+    renderImages(data);
+    const lightbox = new SimpleLightbox('.image-link', {
+        captionsData: "alt"
+    });
+    lightbox.open('show.simplelightbox', () => {
+        gallery.addEventListener('click', (e) => {
+            e.preventDefault();
+        });
+    });
+
+ }
+    
+
+function renderImages(data) {
+    const totalImages = data.totalHits;
+    console.log(totalImages);
+    const markup = data.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+        <a href=${largeImageURL} target="_blank" class="image-link">
+        <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="gallery-img"/>
+  <div class="info">
+    <p class="info-item"> 
+      <b>Likes</b>
+      <span class="count-item">${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+       <span class="count-item">${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span class="count-item">${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span class="count-item">${downloads}</span>
+    </p>
+  </div>
+</div>
+</a>`
+    }).join("");
+    gallery.insertAdjacentHTML("beforeend", markup);
+};
+
+loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
+
+function onClickLoadMoreBtn() {
+    loadMoreBtn.classList.add('js-invisible');
+    page += 1;
+    getImages(searchTerm, page);
+    loadMoreBtn.classList.remove('js-invisible');
+
+}
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,19 +1,29 @@
+import axios from 'axios';
+
 const API_KEY = '4959719-2280f709437ef1ad4f05c2184';
 const BASE_URL = 'https://pixabay.com/api/';
 
+axios.defaults.baseURL = BASE_URL;
 
 
-function fetchImage(serchTerm) {
-    const params = new URLSearchParams({
-    key: API_KEY,
-    q: serchTerm,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true,
-    page: 1,
-    per_page: 40
-});
-    return fetch(`${BASE_URL}?${params}`).then(response => response.json());
+async function fetchImages(serchTerm, page) {
+    try {
+        const { data } = await axios("", {
+            params: {
+            key: API_KEY,
+            q: serchTerm,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: true,
+            page,
+            per_page: 40,
+        }
+        });
+        return data;    
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
-fetchImage("dog").then(data => console.log(data));
+export { fetchImages };
